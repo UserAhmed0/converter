07bdf203e6234912d0a645da0f443c5a25651795
UserAhmed0 <ahmedugurciev84@gmail.com>
2024-05-21 09:53:30 +0300

    first comm

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import { Block } from './Block';
 import './index.scss';
 
 function App() {
+  const [fromCurrency,setFromCurrency] = React.useState('RUB');
+  const [fromPrice, setFromPrice] = React.useState(0);
+  const onChangeFromPrice = (value) => {
+    const price = value / 90;
+    setFromPrice(value);
+    const div = document.querySelector('.emptyblock');
+    div.textContent = price;
+  }
+
+  
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block value={fromPrice} currency={fromCurrency} onChangeCurrency={setFromCurrency} onChangeValue={onChangeFromPrice} />
+      <div className='cont'>
+          <div className='p'>Результат долларах</div>
+          <div className='emptyblock'>0</div>
+      </div>
+
     </div>
   );
 }
